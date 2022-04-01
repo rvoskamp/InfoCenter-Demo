48c2aa16f39679a78272b6db7bcec18b72340ea2
rvoskamp <95710327+rvoskamp@users.noreply.github.com>
2022-04-01 12:41:05 -0400

    Update view.js
    Fixing path issue

--- src/views/view.js
+++ src/views/view.js
@@ -1,6 +1,6 @@
 import { HomeComponent } from './home.js';
 import { LoginComponent } from './login.js';
 import { ListComponent } from './list.js';
-import { ListItemComponent } from './listitem.js';
+import { ListItemComponent } from '../widgets/listitem.js';
 
 export class ViewsModule {}
